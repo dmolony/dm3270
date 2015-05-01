50524d5d1a21ea2afb6d5739bd7012c98245d177
Denis Molony <dmolony@iinet.net.au>
2015-05-01 20:42:25 +1000

    plugin replies

--- src/com/bytezone/dm3270/plugins/PluginResult.java
+++ src/com/bytezone/dm3270/plugins/PluginResult.java
@@ -1,6 +1,14 @@
 package com.bytezone.dm3270.plugins;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PluginResult
 {
+  List<ScreenAction> actions = new ArrayList<> ();
 
+  public void add (ScreenAction action)
+  {
+    actions.add (action);
+  }
 }
