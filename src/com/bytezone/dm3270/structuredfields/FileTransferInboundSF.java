60b866714ad7870abc94ed2175507214662b4bfb
Denis Molony <dmolony@iinet.net.au>
2015-05-21 09:28:10 +1000

    more IND$FILE code

--- src/com/bytezone/dm3270/structuredfields/FileTransferInboundSF.java
+++ src/com/bytezone/dm3270/structuredfields/FileTransferInboundSF.java
@@ -1,32 +1,85 @@
 package com.bytezone.dm3270.structuredfields;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.bytezone.dm3270.application.Utility;
 import com.bytezone.dm3270.display.Screen;
 
 public class FileTransferInboundSF extends StructuredField
 {
+  private final byte rectype;
+  private final byte subtype;
+
+  private byte[] transfer;
+  private boolean ebcdic;
+  private final List<String> extraBytes = new ArrayList<> ();
 
   public FileTransferInboundSF (byte[] buffer, int offset, int length, Screen screen)
   {
     super (buffer, offset, length, screen);
 
-    System.out.println ();
+    assert data[0] == (byte) 0xD0;
+
+    rectype = data[1];
+    subtype = data[2];
+
+    switch (rectype)
+    {
+      case 0x00:
+        extraBytes.add (Utility.toHexString (data, 3, data.length - 3));
+        break;
+
+      case 0x41:
+        extraBytes.add (Utility.toHexString (data, 3, data.length - 3));
+        break;
+
+      case 0x46:
+        if (subtype == 0x05)        // transfer buffer
+        {
+          int buflen = Utility.unsignedShort (data, 12) - 5;
+          extraBytes.add (Utility.toHexString (data, 3, 6));
+          extraBytes.add (Utility.toHexString (data, 9, 5));    // 5 extra
+
+          ebcdic = true;
+          transfer = new byte[buflen];
+          System.arraycopy (data, 14, transfer, 0, buflen);
+        }
+        else
+          extraBytes.add (Utility.toHexString (data, 3, data.length - 3));
+        break;
+
+      case 0x47:
+        extraBytes.add (Utility.toHexString (data, 3, data.length - 3));
+        break;
+
+      default:
+        extraBytes.add (Utility.toHexString (data, 3, data.length - 3));
+        System.out.printf ("Unknown type: %02X%n", rectype);
+    }
+
     System.out.println (this);
     System.out.println ("-----------------------------------------"
         + "------------------------------");
   }
 
   @Override
-  public void process ()
-  {
-    // what to do?
-  }
-
-  @Override
   public String toString ()
   {
     StringBuilder text = new StringBuilder ("Struct Field : D0 File Transfer Inbound\n");
-    text.append (Utility.toHex (data));
+    text.append (String.format ("   type      : %02X%n", rectype));
+    text.append (String.format ("   subtype   : %02X", subtype));
+
+    for (String extra : extraBytes)
+      if (!extra.isEmpty ())
+        text.append ("\n   extras    : " + extra);
+
+    if (transfer != null)
+    {
+      text.append ("\n");
+      text.append (Utility.toHex (transfer, ebcdic));
+    }
+
     return text.toString ();
   }
 }
--- src/com/bytezone/dm3270/structuredfields/FileTransferOutbound.java
+++ src/com/bytezone/dm3270/structuredfields/FileTransferOutbound.java
@@ -1,88 +1,91 @@
 package com.bytezone.dm3270.structuredfields;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.bytezone.dm3270.application.Utility;
 import com.bytezone.dm3270.display.Screen;
 
 public class FileTransferOutbound extends StructuredField
 {
-  private boolean ebcdic;
-  private final byte type;
+  private final byte rectype;
   private final byte subtype;
-  private int max;
-  private String message;
+
   private byte[] transfer;
-  private byte[] extras;
+  private boolean ebcdic;
+  private String message;
+  private final List<String> extraBytes = new ArrayList<> ();
 
   public FileTransferOutbound (byte[] buffer, int offset, int length, Screen screen)
   {
     super (buffer, offset, length, screen);
     assert data[0] == (byte) 0xD0;
 
-    type = data[1];
+    rectype = data[1];
     subtype = data[2];
 
-    if (type == 0x47 && subtype == 0x04)
-      ebcdic = data[6] != 0;                // temp
-
-    switch (type)
+    switch (rectype)
     {
       case 0x00:
-        //        assert data.length == 0x21;
         if (data.length == 33)
         {
           message = new String (data, 26, 7);
-          extras = new byte[23];
-          System.arraycopy (data, 3, extras, 0, extras.length);
+          extraBytes.add (Utility.toHexString (data, 3, 23));
         }
         else if (data.length == 39)
         {
           message = new String (data, 32, 7);
-          extras = new byte[29];
-          System.arraycopy (data, 3, extras, 0, extras.length);
+          extraBytes.add (Utility.toHexString (data, 3, 29));
         }
         else
         {
           System.out.printf ("Unrecognised data length: %d%n", data.length);
           System.out.println (Utility.toHex (data));
+          extraBytes.add (Utility.toHexString (data, 3, data.length - 3));
         }
         break;
 
       case 0x41:
         break;
 
+      case 0x45:
+        break;
+
+      case 0x46:
+        break;
+
       case 0x47:
-        if (subtype == 0x04)        // message
+        if (subtype == 0x04)        // message or transfer buffer
         {
           int buflen = Utility.unsignedShort (data, 6) - 5;
-          extras = new byte[3];
-          System.arraycopy (data, 3, extras, 0, extras.length);
           if (data[6] == 0)       // temp
+          {
+            extraBytes.add (Utility.toHexString (data, 3, 5));
             message = new String (data, 8, buflen).trim ();
+          }
           else
           {
+            extraBytes.add (Utility.toHexString (data, 3, 5));
+            ebcdic = true;
             transfer = new byte[buflen];
             System.arraycopy (data, 8, transfer, 0, buflen);
           }
         }
         else if (subtype == 0x11)   // transfer buffer
-        {
-          extras = new byte[5];
-          System.arraycopy (data, 3, extras, 0, extras.length);
-        }
-        //        else if (subtype == 0x05)   // end of transfer buffers
-        //        {
-        //          extras = new byte[6];
-        //          System.arraycopy (data, 3, extras, 0, extras.length);
-        //        }
+          extraBytes.add (Utility.toHexString (data, 3, data.length - 3));
+        else if (subtype == 0x05)   // end of transfer buffers
+          extraBytes.add (Utility.toHexString (data, 3, data.length - 3));
         else
+        {
+          extraBytes.add (Utility.toHexString (data, 3, data.length - 3));
           System.out.println ("Unknown subtype");
+        }
         break;
 
       default:
-        System.out.printf ("Unknown type: %02X%n", type);
+        System.out.printf ("Unknown type: %02X%n", rectype);
     }
 
-    System.out.println ();
     System.out.println (this);
     System.out.println ("-----------------------------------------"
         + "------------------------------");
@@ -91,7 +94,7 @@ public class FileTransferOutbound extends StructuredField
   @Override
   public void process ()
   {
-    switch (type)
+    switch (rectype)
     {
       case 0x00:                      // get ready for a message or data transfer
         if (subtype == 0x12)
@@ -120,25 +123,22 @@ public class FileTransferOutbound extends StructuredField
   public String toString ()
   {
     StringBuilder text = new StringBuilder ("Struct Field : D0 File Transfer Outbound\n");
-    text.append (String.format ("   type      : %02X%n", type));
-    text.append (String.format ("   subtype   : %02X%n", subtype));
+    text.append (String.format ("   type      : %02X%n", rectype));
+    text.append (String.format ("   subtype   : %02X", subtype));
+
     if (message != null)
-      text.append (String.format ("   message   : %s", message));
-    else if (transfer != null)
+      text.append (String.format ("\n   message   : %s", message));
+
+    for (String extra : extraBytes)
+      if (!extra.isEmpty ())
+        text.append ("\n   extras    : " + extra);
+
+    if (transfer != null)
     {
       text.append ("\n");
       text.append (Utility.toHex (transfer, ebcdic));
     }
-    else
-    {
-      text.append ("\n");
-      text.append (Utility.toHex (data, 3, data.length - 3, ebcdic));
-    }
-    if (extras != null)
-    {
-      text.append ("\n   extras   " + " :\n");
-      text.append (Utility.toHex (extras));
-    }
+
     return text.toString ();
   }
 }
