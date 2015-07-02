b6a371013113b90b47ecce2d0350750fd2d3579f
Denis Molony <dmolony@iinet.net.au>
2015-07-03 08:56:22 +1000

    added menubar to FileStage

--- src/com/bytezone/dm3270/filetransfer/FileStage.java
+++ src/com/bytezone/dm3270/filetransfer/FileStage.java
@@ -21,11 +21,17 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TabPane.TabClosingPolicy;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
@@ -33,6 +39,8 @@ import javafx.stage.Stage;
 
 public class FileStage extends Stage implements TSOCommandStatusListener
 {
+  private final static String OS = System.getProperty ("os.name");
+  private final static boolean SYSTEM_MENUBAR = OS != null && OS.startsWith ("Mac");
   private static final int PAGE_SIZE = 66;
 
   private final TabPane tabPane = new TabPane ();
@@ -41,8 +49,6 @@ public class FileStage extends Stage implements TSOCommandStatusListener
   private final WindowSaver windowSaver;
 
   private final Button btnHide = new Button ("Hide Window");
-  private final Button btnSave = new Button ("Save");
-  private final Button btnPrint = new Button ("Print");
 
   private final Label lblLineSize = new Label ("Line size");
   private final Label lblPageSize = new Label ("Page size");
@@ -58,7 +64,7 @@ public class FileStage extends Stage implements TSOCommandStatusListener
   private final CheckBox chkASCII = new CheckBox ();
   private final TextField txtTotalLines = new TextField ();
 
-  private final boolean javaPrintingSucks = false;
+  private final MenuBar menuBar = new MenuBar ();
 
   public FileStage (Preferences prefs)
   {
@@ -71,15 +77,9 @@ public class FileStage extends Stage implements TSOCommandStatusListener
 
     HBox buttonBox = new HBox (10);
     btnHide.setPrefWidth (100);
-    btnSave.setPrefWidth (100);
-    btnPrint.setPrefWidth (100);
 
     buttonBox.setAlignment (Pos.CENTER_RIGHT);
     buttonBox.setPadding (new Insets (10, 10, 10, 10));           // trbl
-
-    if (!javaPrintingSucks)
-      buttonBox.getChildren ().add (btnPrint);
-    buttonBox.getChildren ().addAll (btnSave, btnHide);
 
     HBox optionsBox = new HBox (10);
     optionsBox.setAlignment (Pos.CENTER_LEFT);
@@ -102,46 +102,18 @@ public class FileStage extends Stage implements TSOCommandStatusListener
     bottomBorderPane.setLeft (optionsBox);
     bottomBorderPane.setRight (buttonBox);
 
-    btnHide.setOnAction (e -> hide ());
-    btnSave.setOnAction (e -> save ());
-    // btnPrint.setOnAction (e -> print ());
+    menuBar.getMenus ().addAll (getFileMenu ());
 
-    btnPrint.setOnAction (new EventHandler<ActionEvent> ()
-    {
-      @Override
-      public void handle (ActionEvent e)
-      {
-        FileTab fileTab = getSelectedTab ();
-        if (fileTab == null)
-          return;
+    BorderPane topBorderPane = new BorderPane ();
+    topBorderPane.setTop (menuBar);
+    if (SYSTEM_MENUBAR)
+      menuBar.useSystemMenuBarProperty ().set (true);
 
-        SwingUtilities.invokeLater (new Runnable ()
-        {
-          @Override
-          public void run ()
-          {
-            java.awt.print.PrinterJob printerJob =
-                java.awt.print.PrinterJob.getPrinterJob ();
-
-            // printerJob.pageDialog ();
-            if (printerJob.printDialog ())
-            {
-              printerJob.setPrintable (fileTab.report);
-              try
-              {
-                printerJob.print ();
-              }
-              catch (PrinterException e)
-              {
-                e.printStackTrace ();
-              }
-            }
-          }
-        });
-      }
-    });
+    btnHide.setOnAction (e -> closeWindow ());
+    setOnCloseRequest (e -> closeWindow ());
 
     BorderPane borderPane = new BorderPane ();
+    borderPane.setTop (topBorderPane);
     borderPane.setCenter (tabPane);
     borderPane.setBottom (bottomBorderPane);
 
@@ -151,10 +123,82 @@ public class FileStage extends Stage implements TSOCommandStatusListener
     if (!windowSaver.restoreWindow ())
       centerOnScreen ();
 
-    setOnCloseRequest (e -> closeWindow ());
-
     tabPane.getSelectionModel ().selectedItemProperty ()
         .addListener ( (obs, oldTab, newTab) -> select ((FileTab) newTab));
+  }
+
+  private Menu getFileMenu ()
+  {
+    Menu menuFile = new Menu ("File");
+
+    MenuItem menuItemOpen = getMenuItem ("Open...", e -> openFile (), KeyCode.O);
+    MenuItem menuItemSave = getMenuItem ("Save...", e -> saveFile (), KeyCode.S);
+    MenuItem menuItemPrint = getMenuItem ("Print", e -> printFile (), KeyCode.P);
+    MenuItem menuItemClose = getMenuItem ("Close window", e -> closeWindow (), KeyCode.W);
+
+    menuFile.getItems ().addAll (menuItemOpen, menuItemSave, menuItemPrint,
+                                 menuItemClose);
+
+    return menuFile;
+  }
+
+  private MenuItem getMenuItem (String text, EventHandler<ActionEvent> eventHandler,
+      KeyCode keyCode)
+  {
+    MenuItem menuItem = new MenuItem (text);
+    menuItem.setOnAction (eventHandler);
+    menuItem
+        .setAccelerator (new KeyCodeCombination (keyCode, KeyCombination.SHORTCUT_DOWN));
+    return menuItem;
+  }
+
+  private void openFile ()
+  {
+
+  }
+
+  private void printFile ()
+  {
+    FileTab fileTab = getSelectedTab ();
+    if (fileTab == null)
+      return;
+
+    SwingUtilities.invokeLater (new Runnable ()
+    {
+      @Override
+      public void run ()
+      {
+        java.awt.print.PrinterJob printerJob = java.awt.print.PrinterJob.getPrinterJob ();
+
+        // printerJob.pageDialog ();
+        if (printerJob.printDialog ())
+        {
+          printerJob.setPrintable (fileTab.report);
+          try
+          {
+            printerJob.print ();
+          }
+          catch (PrinterException e)
+          {
+            e.printStackTrace ();
+          }
+        }
+      }
+    });
+  }
+
+  private void saveFile ()
+  {
+    FileTab fileTab = getSelectedTab ();
+    if (fileTab == null)
+      return;
+
+    System.out.println ("Save:  " + fileTab.getTitle ());
+  }
+
+  private FileTab getSelectedTab ()
+  {
+    return (FileTab) tabPane.getSelectionModel ().getSelectedItem ();
   }
 
   private void select (FileTab tab)
@@ -185,20 +229,6 @@ public class FileStage extends Stage implements TSOCommandStatusListener
   {
     windowSaver.saveWindow ();
     hide ();
-  }
-
-  private void save ()
-  {
-    FileTab fileTab = getSelectedTab ();
-    if (fileTab == null)
-      return;
-
-    System.out.println ("Save:  " + fileTab.getTitle ());
-  }
-
-  private FileTab getSelectedTab ()
-  {
-    return (FileTab) tabPane.getSelectionModel ().getSelectedItem ();
   }
 
   public Transfer openTransfer (FileTransferOutboundSF transferRecord)
