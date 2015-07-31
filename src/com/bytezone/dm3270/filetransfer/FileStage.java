273e67a681d87cd0508406a039da5de7b895dd0d
Denis Molony <dmolony@iinet.net.au>
2015-07-31 10:49:16 +1000

    connected to Reporter

--- src/com/bytezone/dm3270/filetransfer/FileStage.java
+++ src/com/bytezone/dm3270/filetransfer/FileStage.java
@@ -8,7 +8,9 @@ import com.bytezone.dm3270.application.WindowSaver;
 import com.bytezone.dm3270.display.ScreenDetails;
 import com.bytezone.dm3270.display.TSOCommandStatusListener;
 import com.bytezone.reporter.application.ReporterScene;
+import com.bytezone.reporter.application.TreePanel;
 
+import javafx.application.Platform;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -17,13 +19,14 @@ public class FileStage extends Stage implements TSOCommandStatusListener
   private final List<Transfer> transfers = new ArrayList<> ();
   private Transfer currentTransfer;
   private final WindowSaver windowSaver;
+  private final ReporterScene reporterScene;
 
   public FileStage (Preferences prefs)
   {
     setTitle ("Report display");
 
     BorderPane borderPane = new BorderPane ();
-    ReporterScene reporterScene = new ReporterScene (prefs, borderPane);
+    reporterScene = new ReporterScene (prefs, borderPane);
 
     setScene (reporterScene);
     setOnCloseRequest (e -> closeWindow ());
@@ -38,8 +41,10 @@ public class FileStage extends Stage implements TSOCommandStatusListener
       return;
 
     transfers.add (transfer);
+    TreePanel treePanel = reporterScene.getTreePanel ();
 
-    //    Platform.runLater ( () -> tabPane.getTabs ().add (tab));
+    Platform.runLater ( () -> treePanel.addBuffer (transfer.getFileName (),
+                                                   transfer.combineDataBuffers ()));
   }
 
   private void closeWindow ()
