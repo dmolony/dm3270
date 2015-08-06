e5c809524a7dd6ee211eca906ab72549ce57ab89
Denis Molony <dmolony@iinet.net.au>
2015-08-07 05:56:34 +1000

    deleted old code

--- src/com/bytezone/dm3270/filetransfer/FileTransferInboundSF.java
+++ src/com/bytezone/dm3270/filetransfer/FileTransferInboundSF.java
@@ -35,50 +35,6 @@ public class FileTransferInboundSF extends FileTransferSF
       ptr += transferRecord.length ();
     }
 
-    //    switch (rectype)
-    //    {
-    //      case 0x00:// acknowledge OPEN
-    //        // subtype 0x09
-    //        if (data.length != 3)
-    //          System.out.printf ("Unrecognised data length: %d%n", data.length);
-    //        break;
-    //
-    //      case 0x41:// acknowledge CLOSE
-    //        // subtype 0x09
-    //        if (data.length != 3)
-    //          System.out.printf ("Unrecognised data length: %d%n", data.length);
-    //        break;
-    //
-    //      case 0x46:
-    //        if (subtype == 0x05) // transfer buffer to host
-    //        {
-    //          transferRecords.add (new RecordNumber (data, 3));
-    //          transferRecords.add (new DataRecord (data, 9));
-    //        }
-    //        else if (subtype == 0x08) // no data
-    //        {
-    //          transferRecords.add (new ErrorRecord (data, 3));
-    //          if (data.length != 7)
-    //            System.out.printf ("Unrecognised data length: %d%n", data.length);
-    //        }
-    //        else if (data.length != 3)
-    //          System.out.printf ("Unrecognised data length: %d%n", data.length);
-    //        break;
-    //
-    //      case 0x47:// acknowledge DATA received
-    //        // subtype 0x05
-    //        transferRecords.add (new RecordNumber (data, 3));
-    //
-    //        if (data.length != 9)
-    //          System.out.printf ("Unrecognised data length: %d%n", data.length);
-    //        break;
-    //
-    //      default:
-    //        if (data.length > 3)
-    //          transferRecords.add (new TransferRecord (data, 3));
-    //        System.out.printf ("Unknown type: %02X%n", rectype);
-    //    }
-
     if (debug)
     {
       System.out.println (this);
