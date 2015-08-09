4639e826ad5eab4c044770761068a6d5f31924f1
Denis Molony <dmolony@iinet.net.au>
2015-08-09 18:58:59 +1000

    Dataset table

--- src/com/bytezone/dm3270/datasets/Dataset.java
+++ src/com/bytezone/dm3270/datasets/Dataset.java
@@ -1,11 +1,23 @@
 package com.bytezone.dm3270.datasets;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class Dataset
 {
   private StringProperty propertyDatasetName;
+  private IntegerProperty propertyTracks;
+  private IntegerProperty propertyCylinders;
+  private IntegerProperty propertyExtents;
+  private IntegerProperty propertyPercentUsed;
+  private StringProperty propertyVolume;
+
+  public Dataset (String name)
+  {
+    propertyDatasetName.set (name);
+  }
 
   // DatasetName
 
@@ -24,5 +36,100 @@ public class Dataset
     if (propertyDatasetName == null)
       propertyDatasetName = new SimpleStringProperty (this, "DatasetName");
     return propertyDatasetName;
+  }
+
+  // Tracks
+
+  public void setTracks (int value)
+  {
+    propertyTracks ().set (value);
+  }
+
+  public int getTracks ()
+  {
+    return propertyTracks ().get ();
+  }
+
+  public IntegerProperty propertyTracks ()
+  {
+    if (propertyTracks == null)
+      propertyTracks = new SimpleIntegerProperty (this, "Tracks");
+    return propertyTracks;
+  }
+
+  // Cylinders
+
+  public void setCylinders (int value)
+  {
+    propertyCylinders ().set (value);
+  }
+
+  public int getCylinders ()
+  {
+    return propertyCylinders ().get ();
+  }
+
+  public IntegerProperty propertyCylinders ()
+  {
+    if (propertyCylinders == null)
+      propertyCylinders = new SimpleIntegerProperty (this, "Cylinders");
+    return propertyCylinders;
+  }
+
+  // Extents
+
+  public void setExtents (int value)
+  {
+    propertyExtents ().set (value);
+  }
+
+  public int getExtents ()
+  {
+    return propertyExtents ().get ();
+  }
+
+  public IntegerProperty propertyExtents ()
+  {
+    if (propertyExtents == null)
+      propertyExtents = new SimpleIntegerProperty (this, "Extents");
+    return propertyExtents;
+  }
+
+  // PercentUsed
+
+  public void setPercentUsed (int value)
+  {
+    propertyPercentUsed ().set (value);
+  }
+
+  public int getPercentUsed ()
+  {
+    return propertyPercentUsed ().get ();
+  }
+
+  public IntegerProperty propertyPercentUsed ()
+  {
+    if (propertyPercentUsed == null)
+      propertyPercentUsed = new SimpleIntegerProperty (this, "PercentUsed");
+    return propertyPercentUsed;
+  }
+
+  // Volume
+
+  public void setVolume (String value)
+  {
+    propertyVolume ().set (value);
+  }
+
+  public String getVolume ()
+  {
+    return propertyVolume ().get ();
+  }
+
+  public StringProperty propertyVolume ()
+  {
+    if (propertyVolume == null)
+      propertyVolume = new SimpleStringProperty (this, "Volume");
+    return propertyVolume;
   }
 }
--- src/com/bytezone/dm3270/datasets/DatasetTable.java
+++ src/com/bytezone/dm3270/datasets/DatasetTable.java
@@ -20,6 +20,36 @@ public class DatasetTable extends TableView<Dataset>
     colDatasetName.setCellValueFactory (new PropertyValueFactory<> ("DatasetName"));
     getColumns ().add (colDatasetName);
 
+    TableColumn<Dataset, Integer> colTracks = new TableColumn<> ("Tracks");
+    colTracks.setPrefWidth (100);
+    colTracks.setCellValueFactory (new PropertyValueFactory<> ("Tracks"));
+    getColumns ().add (colTracks);
+
+    TableColumn<Dataset, Integer> colCylinders = new TableColumn<> ("Cylinders");
+    colCylinders.setPrefWidth (100);
+    colCylinders.setCellValueFactory (new PropertyValueFactory<> ("Cylinders"));
+    getColumns ().add (colCylinders);
+
+    TableColumn<Dataset, Integer> colExtents = new TableColumn<> ("Extents");
+    colExtents.setPrefWidth (100);
+    colExtents.setCellValueFactory (new PropertyValueFactory<> ("Extents"));
+    getColumns ().add (colExtents);
+
+    TableColumn<Dataset, Integer> colPercentUsed = new TableColumn<> ("Percent used");
+    colPercentUsed.setPrefWidth (100);
+    colPercentUsed.setCellValueFactory (new PropertyValueFactory<> ("PercentUsed"));
+    getColumns ().add (colPercentUsed);
+
+    TableColumn<Dataset, String> colVolume = new TableColumn<> ("Volume");
+    colVolume.setPrefWidth (100);
+    colVolume.setCellValueFactory (new PropertyValueFactory<> ("Volume"));
+    getColumns ().add (colVolume);
+
     setItems (datasets);
+  }
+
+  public void addDataset (Dataset dataset)
+  {
+    datasets.add (dataset);
   }
 }
