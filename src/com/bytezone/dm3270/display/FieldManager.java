8afbe5e49481b09c7f52671fc38462bbbd828f88
Denis Molony <dmolony@iinet.net.au>
2015-10-24 05:50:09 +1100

    doco

--- src/com/bytezone/dm3270/display/FieldManager.java
+++ src/com/bytezone/dm3270/display/FieldManager.java
@@ -53,36 +53,34 @@ public class FieldManager
     int first = -1;
     int ptr = 0;
 
-    while (ptr != first)// not wrapped around to the first field yet
+    while (ptr != first)            // not wrapped around to the first field yet
     {
       ScreenPosition screenPosition = screen.getScreenPosition (ptr);
 
-      // check for the start of a new field
-      if (screenPosition.isStartField ())
+      if (screenPosition.isStartField ())   // check for the start of a new field
       {
-        if (start >= 0)
-        // if there is a field to add
+        if (start >= 0)                     // if there is a field to add
+
         {
           addField (new Field (screen, positions));
           positions.clear ();
         }
         else
-          first = ptr;// this is the first field on the screen
+          first = ptr;                      // this is the first field on the screen
 
-        start = ptr;// beginning of the current field
+        start = ptr;                        // beginning of the current field
       }
 
       // add ScreenPosition to the current field
-      if (start >= 0)
-        // if we are in a field...
-        positions.add (screenPosition);// collect next field's positions
+      if (start >= 0)                       // if we are in a field...
+        positions.add (screenPosition);     // collect next field's positions
 
       // increment ptr and wrap around
-      if (++ptr == screen.screenSize)
-      {// faster than validate()
+      if (++ptr == screen.screenSize)       // faster than validate()
+      {
         ptr = 0;
         if (first == -1)
-          break;// wrapped around and still no fields
+          break;                            // wrapped around and still no fields
       }
     }
 
