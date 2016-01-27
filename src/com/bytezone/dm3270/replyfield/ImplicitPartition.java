297b4620833261495ef6dfe755be4b7a45a6a647
Denis Molony <dmolony@iinet.net.au>
2016-01-27 19:11:10 +1100

    tidying

--- src/com/bytezone/dm3270/replyfield/ImplicitPartition.java
+++ src/com/bytezone/dm3270/replyfield/ImplicitPartition.java
@@ -1,11 +1,14 @@
 package com.bytezone.dm3270.replyfield;
 
+import com.bytezone.dm3270.display.ScreenDimensions;
 import com.bytezone.dm3270.utilities.Dm3270Utility;
 
 public class ImplicitPartition extends QueryReplyField
 {
   int width, height;
   int alternateWidth, alternateHeight;
+  private ScreenDimensions implicitScreenDimensions;
+  private ScreenDimensions alternateScreenDimensions;
 
   public ImplicitPartition ()
   {
@@ -13,26 +16,17 @@ public class ImplicitPartition extends QueryReplyField
 
     int ptr = createReply (13);
 
-    /* Bytes 4-5 Reserved */
     reply[ptr++] = 0x00;
     reply[ptr++] = 0x00;
 
-    /* 6.31.3 Implicit Partition Sizes for Display Devices Self-Defining Parameter */
     reply[ptr++] = 0x0B;
     reply[ptr++] = 0x01;
     reply[ptr++] = 0x00;
 
-    /* Bytes 9-10   Width of the Implicit Partition default screen size */
-    ptr = Dm3270Utility.packUnsignedShort (0x50, reply, ptr);
-
-    /* Bytes 11-12  Height of the Implicit Partition default screen size */
-    ptr = Dm3270Utility.packUnsignedShort (0x18, reply, ptr);
-
-    /* Bytes 13-14  Width of the Implicit Partition alternate screen size */
-    ptr = Dm3270Utility.packUnsignedShort (0x50, reply, ptr);
-
-    /* Bytes 15-16  Height of the Implicit Partition alternate screen size */
-    ptr = Dm3270Utility.packUnsignedShort (0x18, reply, ptr);
+    ptr = Dm3270Utility.packUnsignedShort (0x50, reply, ptr);     // width
+    ptr = Dm3270Utility.packUnsignedShort (0x18, reply, ptr);     // height
+    ptr = Dm3270Utility.packUnsignedShort (0x50, reply, ptr);     // alt width
+    ptr = Dm3270Utility.packUnsignedShort (0x18, reply, ptr);     // alt height
 
     checkDataLength (ptr);
   }
@@ -48,9 +42,22 @@ public class ImplicitPartition extends QueryReplyField
     alternateWidth = Dm3270Utility.unsignedShort (data, 11);
     alternateHeight = Dm3270Utility.unsignedShort (data, 13);
 
+    implicitScreenDimensions = new ScreenDimensions (height, width);
+    alternateScreenDimensions = new ScreenDimensions (alternateHeight, alternateWidth);
+
     if (alternateHeight != 24 || alternateWidth != 80)
       System.out.printf ("Alternate screen height:%d, width:%d%n", alternateHeight,
                          alternateWidth);
+  }
+
+  public ScreenDimensions getScreenDimensions ()
+  {
+    return implicitScreenDimensions;
+  }
+
+  public ScreenDimensions getAlternateScreenDimensions ()
+  {
+    return alternateScreenDimensions;
   }
 
   @Override
