dee1fdc26d0bbf60dbae4d34eac404bf7ca7508f
Denis Molony <dmolony@iinet.net.au>
2016-02-04 11:31:18 +1100

    doco

--- src/com/bytezone/dm3270/extended/BindCommand.java
+++ src/com/bytezone/dm3270/extended/BindCommand.java
@@ -4,22 +4,6 @@ import com.bytezone.dm3270.display.Screen;
 import com.bytezone.dm3270.display.ScreenDimensions;
 import com.bytezone.dm3270.utilities.Dm3270Utility;
 
-// See SNA Programming pp 807
-
-/*
- * #define TN3270E_UNBIND_NORMAL 0x01
- * #define TN3270E_UNBIND_BIND_FORTHCOMING 0x02
- * #define TN3270E_UNBIND_VR_INOPERATIVE 0x07
- * #define TN3270E_UNBIND_RX_INOPERATIVE 0x08
- * #define TN3270E_UNBIND_HRESET 0x09
- * #define TN3270E_UNBIND_SSCP_GONE 0x0a
- * #define TN3270E_UNBIND_VR_DEACTIVATED 0x0b
- * #define TN3270E_UNBIND_LU_FAILURE_PERM 0x0c
- * #define TN3270E_UNBIND_LU_FAILURE_TEMP 0x0e
- * #define TN3270E_UNBIND_CLEANUP 0x0f
- * #define TN3270E_UNBIND_BAD_SENSE 0xfe
- */
-
 // http://publibfp.dhe.ibm.com/cgi-bin/bookmgr/BOOKS/d50a5007/6.3.20
 // z/OS V2R1.0 Communications Server: SNA Programming
 
